64a1e60c9c66f2fafc52aaed4c0a65d5dc4afe4a
vincent0955 <74017998+vincent0955@users.noreply.github.com>
2021-09-18 11:47:09 -0400

    add time

--- popup.js
+++ popup.js
@@ -1,5 +1,14 @@
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
-    console.log(url);
-    // use `url` here inside the callback because it's asynchronous!
-});
+function a (){
+	var d = new Date();
+	var n2 = d.getTime();
+	console.log(n);
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+		let url = tabs[0].url;
+		console.log(url);
+		let url2 = url.split('/', 3).join('/');
+		console.log(url2);
+		// use `url` here inside the callback because it's asynchronous!
+	});
+	setTimeout(a, 5000);
+}
+a();
