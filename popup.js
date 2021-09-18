7f883ec0cda98acb34f4ff323b092892862f64ed
andrew-33 <andrew123709@gmail.com>
2021-09-18 12:17:40 -0400

    html

--- popup.js
+++ popup.js
@@ -1,12 +1,11 @@
 function a (){
-	var d = new Date();
-	var n2 = d.getTime();
-	console.log(n);
 	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
 		let url = tabs[0].url;
 		console.log(url);
 		let url2 = url.split('/', 3).join('/');
 		console.log(url2);
+		document.getElementById('url').href = url2;
+		document.getElementById('urltext').innerHTML = url2;
 		// use `url` here inside the callback because it's asynchronous!
 	});
 	setTimeout(a, 5000);
