0dbdb97ca8094dae0dcde08ae1d581a2a2c6f4eb
vincent0955 <74017998+vincent0955@users.noreply.github.com>
2021-09-18 13:48:14 -0400

    add code

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+const arr = [];
+const timeArr = [];
 function a (){
 	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
 		let url = tabs[0].url;
@@ -6,8 +8,23 @@ function a (){
 		console.log(url2);
 		document.getElementById('url').href = url2;
 		document.getElementById('urltext').innerHTML = url2;
+		
+		console.log(arr.indexOf(url2));
+		if (arr.includes(url2)){
+			timeArr[arr.indexOf(url2)] += 5000;
+		}
+		else{
+			arr.unshift(url2);
+			timeArr.unshift(5000);
+		}
+		
+		
+		//timeArr[0] += 5000;
+		
 		// use `url` here inside the callback because it's asynchronous!
 	});
+	console.log(arr);
+	console.log(timeArr);
 	setTimeout(a, 5000);
 }
 a();
