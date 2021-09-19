60b277611e19edcd15aa7672426378cc6ca9ad02
vincent0955 <74017998+vincent0955@users.noreply.github.com>
2021-09-19 09:25:54 -0400

    bugfix

--- background.js
+++ background.js
@@ -1,3 +1,29 @@
+function bugfix () {
+    var arr = [];
+    var timeArr = [];
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+
+        let url = tabs[0].url;
+        let url2 = url.split('/', 3)[2];
+
+
+            if (arr.includes(url2)) {
+                timeArr[arr.indexOf(url2)] += 1;
+            }
+            else {
+                arr.unshift(url2);
+                timeArr.unshift(1);
+            }
+
+            chrome.storage.local.set({"key": arr}, function() {
+                console.log('Value is set to ' + arr);
+            });
+            chrome.storage.local.set({"tkey": timeArr}, function() {
+                console.log('Value is set to ' + timeArr);
+            });
+
+    })
+}
 
 function a () {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
@@ -6,6 +32,9 @@ function a () {
         var arr = [];
         var timeArr = [];
         chrome.storage.local.get("key", function(value) {
+            if (value.key == undefined){ //fixes startup bug
+                bugfix();
+            }
             console.log('Initial Value currently is ' + value.key);
             arr = value.key;
             chrome.storage.local.get("tkey", function(value) {
